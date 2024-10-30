8a597e4db198113f55782c63f14ebf45f45a8a70
Akshay C P <com.akshaycp@gmail.com>
2024-10-30 12:13:00 +0530

    Create boxdecor.dart

---
+++ boxdecor.dart
@@ -0,0 +1,12 @@
+decoration: BoxDecoration(
+              color: containerColor,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3), // Shadow color
+                  spreadRadius: 0.2,
+                  blurRadius: 3,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
